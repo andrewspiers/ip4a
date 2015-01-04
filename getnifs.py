5ee351f8e49ec7c34cb7ccf279f6aad8217221d8
Andrew Spiers <andrew@andrewspiers.net>
2015-01-04 21:30:59 +1100

    use python 3 compat print syntax

--- getnifs.py
+++ getnifs.py
@@ -101,5 +101,5 @@ def get_network_interfaces():
         libc.freeifaddrs(ifap)
 
 if __name__ == '__main__':
-    print [str(ni) for ni in get_network_interfaces()]
+    print ([str(ni) for ni in get_network_interfaces()])
 
